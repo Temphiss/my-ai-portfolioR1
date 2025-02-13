3d1f6a18a54fdddaa1b157b25623557def4de0be
Temphiss <61607418+Temphiss@users.noreply.github.com>
2025-02-13 09:05:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add AOS (Animate On Scroll) library
+document.head.innerHTML += '<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">';
+document.body.innerHTML += '<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>';
+AOS.init();
